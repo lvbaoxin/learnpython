3d011272a84ff1ec9e0d6cb93209e8a6f5448b47
DS-20161030HXRC\Administrator <745458741@qq.com>
2020-06-10 21:18:28 +0800

    no message

---
+++ day2.py
@@ -0,0 +1,12 @@
+money = 20
+goods = 8.8
+print(money,type(money)) # type() 查看类型
+print(goods,type(goods))
+print(goods,money,sep="#") # 设置分割符
+print('goods\nmoney') #\n 换行
+print("aaa",end='') # end为空字符串不换行
+print("bbb")
+
+print("亲爱的娜娜:\n","\t请点击链接激活用户：激活用户")   # \t 制表符
+#转义字符： \n 换行  \t 制表符  \'   \"  \r 回车
+print("乔治说: \'想吃冰淇淋\' ")

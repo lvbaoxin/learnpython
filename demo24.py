72f5318c932599e7f184ac4380bb1cfa1b3a73f4
lvbaoxin <745458741@qq.com>
2020-12-17 17:49:37 +0800

    继承及其实现方式

---
+++ demo24.py
@@ -0,0 +1,25 @@
+class Person(object):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def info(self):
+        print(self.name, self.age)
+
+
+class Student(Person):
+    def __init__(self, name, age, stu_no):
+        super().__init__(name, age)
+        self.stu_no = stu_no
+
+
+class Teacher(Person):
+    def __init__(self, name, age, teachofyear):
+        super().__init__(name, age)
+        self.teachofyear = teachofyear
+
+
+stu = Student("张二", 20, 1000)
+teacher = Teacher("张强", 30, 10)
+stu.info()  # 张二 20
+teacher.info()  # 张强 30

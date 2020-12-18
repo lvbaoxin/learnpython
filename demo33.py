e4d740de00fa2846f6acd1f8708edcedc224e095
lvbaoxin <745458741@qq.com>
2020-12-18 15:02:54 +0800

    常用的内置模块

---
+++ demo33.py
@@ -0,0 +1,13 @@
+import urllib.request
+
+print(urllib.request.urlopen("http://www.baidu.com").read())
+# sys:与python解释器及其环境操作相关的标准库
+# time:与时间相关的各种函数的标准库
+# os:提供了访问操作系统服务功能的标准库
+# calendar:提供了日期相关的各种函数标准库
+# urllib:用于读取来自网上的数据标准库
+# json：用于json序列化和反序列化对象
+# re:用于在字符串中执行正则表达式匹配和替换
+# math:提供标准算术运算函数的标准库
+# decimal：用于进行精确控制运算精度、有效数位和四舍五入操作的十进制运算
+# logging:提供了灵活的记录事件、错误、警告、和调式信息等日志信息的功能。
